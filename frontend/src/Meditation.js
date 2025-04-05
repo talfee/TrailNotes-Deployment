8a7d9c24007c99c918c1f0864e940d86236ebd9e
Jessieec <jessie.chx@hotmail.com>
2025-04-05 16:11:23 -0700

    added background image

--- frontend/src/Meditation.js
+++ frontend/src/Meditation.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Timer from './components/timer';
 import BackButton from './components/BackButton';
+import backgroundImage from './images/Generated_Meditation.jpeg';
 
 function Meditation({ goHome }) {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <button onClick={goHome}>Back</button>
-      {/* <h2>Meditation</h2>
-      <p>This is where your meditation tools/videos/etc. will go.</p> */}
+    const pageStyle = {
+        padding: '2rem',
+        height: '100vh',
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        color: '#fff',
+      };
+
+//   return (
+//     <div style={{ padding: '2rem' }}>
+//       <button onClick={goHome}>Back</button>
+//       {/* <h2>Meditation</h2>
+//       <p>This is where your meditation tools/videos/etc. will go.</p> */}
+//       <Timer />
+//     </div>
+//   );
+return (
+    <div style={pageStyle}>
+      <BackButton onClick={goHome} />
       <Timer />
     </div>
   );
