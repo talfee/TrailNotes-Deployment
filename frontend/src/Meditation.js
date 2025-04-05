e9882888a3a21a49321b6023b51857da6e72d316
talfee <88052205+talfee@users.noreply.github.com>
2025-04-05 15:03:26 -0700

    Anagha's timer

--- frontend/src/Meditation.js
+++ frontend/src/Meditation.js
@@ -1,11 +1,27 @@
+// import React from 'react';
+
+// function Meditation({ goHome }) {
+//   return (
+//     <div style={{ padding: '2rem' }}>
+//       <button onClick={goHome}>Back</button>
+//       <h2>Meditation</h2>
+//       <p>This is where your meditation tools/videos/etc. will go.</p>
+//     </div>
+//   );
+// }
+
+// export default Meditation;
+
 import React from 'react';
+import Timer from './components/timer';
 
 function Meditation({ goHome }) {
   return (
     <div style={{ padding: '2rem' }}>
       <button onClick={goHome}>Back</button>
-      <h2>Meditation</h2>
-      <p>This is where your meditation tools/videos/etc. will go.</p>
+      {/* <h2>Meditation</h2>
+      <p>This is where your meditation tools/videos/etc. will go.</p> */}
+      <Timer />
     </div>
   );
 }
