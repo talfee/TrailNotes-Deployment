b6892afb1f972051a4e268d80f37ff4acd7c1ee5
Jessieec <jessie.chx@hotmail.com>
2025-04-05 15:23:47 -0700

    added backbutton into meditation

--- frontend/src/Meditation.js
+++ frontend/src/Meditation.js
@@ -14,11 +14,12 @@
 
 import React from 'react';
 import Timer from './components/timer';
+import BackButton from './components/BackButton';
 
 function Meditation({ goHome }) {
   return (
     <div style={{ padding: '2rem' }}>
-      <button onClick={goHome}>Back</button>
+      <BackButton onClick={goHome} />
       {/* <h2>Meditation</h2>
       <p>This is where your meditation tools/videos/etc. will go.</p> */}
       <Timer />
